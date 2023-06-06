f8c68e57655a7be148dee1d6e72531364af00bd1
Julio Rodriguez Escogido <78891349+JulioRodr17@users.noreply.github.com>
2023-06-06 00:16:00 -0600

    Primer parte del automata para el scanner

--- Analizador Sintactico/Kotlin/Scanner.kt
+++ Analizador Sintactico/Kotlin/Scanner.kt
@@ -9,4 +9,35 @@ class Scanner(private val source: String) {
         keywords["distinct"] = TokenType.DISTINCT
     }
 
+    fun scanTokens(): List<Token> {
+        var state = 0
+        var lexemeStart = 0
+        var i = 0
+        var character: Char?
+        val lexeme = StringBuilder()
+
+        while (i <= source.length) {
+            character = if (i < source.length) source[i] else '\u00A0'
+
+            when (state) {
+                0 -> when {
+                    character == '*' -> addToken(TokenType.ASTERISK, character.toString(), i + 1)
+                    character == ',' -> addToken(TokenType.COMMA, character.toString(), i + 1)
+                    character == '.' -> addToken(TokenType.DOT, character.toString(), i + 1)
+                    character.isLetter() -> {
+                        state = 1; lexeme.append(character); lexemeStart = i
+                    }
+                }
+
+
+            }
+
+            i++
+        }
+
+        addToken(TokenType.EOF, "", source.length)
+        return tokens
+    }
+
+    
 }
