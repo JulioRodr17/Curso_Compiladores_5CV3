b09ffba0778d4c82c310c9afb3c57e09d853bfc4
Julio Rodriguez Escogido <78891349+JulioRodr17@users.noreply.github.com>
2023-06-05 23:42:37 -0600

    Terminacion de las palabras reservadas
    Agregadas al archivo de TokenType

--- Analizador Sintactico/Kotlin/TokenType.kt
+++ Analizador Sintactico/Kotlin/TokenType.kt
@@ -2,5 +2,16 @@ enum class TokenType {
     // Identificadores
     IDENTIFIER,
 
-    
+    // Palabaras reservadas
+    SELECT,
+    FROM,
+    DISTINCT,
+
+    // Carácteres
+    COMMA,
+    DOT,
+    ASTERISK,
+
+    // Final de cadena
+    EOF,
 }
