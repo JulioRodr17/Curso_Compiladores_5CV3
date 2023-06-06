52860da3bae780ff522c9c5e844ed9dda341c009
Julio Rodriguez Escogido <78891349+JulioRodr17@users.noreply.github.com>
2023-06-06 00:17:56 -0600

    Creacion de la funcion Main

---
+++ Analizador Sintactico/Kotlin/Main.kt
@@ -0,0 +1,32 @@
+var existErrors = false
+
+fun main(args: Array<String>) {
+    runPrompt()
+}
+
+fun runPrompt() {
+    while (true) {
+        print("> ")
+        readlnOrNull()?.let {
+            run(it)
+        }
+    }
+}
+
+fun run(source: String) {
+    val scanner = Scanner(source)
+    val tokens = scanner.scanTokens()
+
+    for (token in tokens) {
+        println(token)
+    }
+}
+
+fun error(line: Int, message: String) {
+    report(line, "", message)
+}
+
+fun report(line: Int, where: String, message: String) {
+    println("[line $line] Error $where: $message")
+    existErrors = true
+}
