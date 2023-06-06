ba6635f687683f4bb5acf697406ca4385ead4b00
Julio Rodriguez Escogido <78891349+JulioRodr17@users.noreply.github.com>
2023-06-06 01:22:11 -0600

    Agregue simbolos y tipo de acciones

--- Analizador Sintactico/Ascendente/Parser.kt
+++ Analizador Sintactico/Ascendente/Parser.kt
@@ -2,7 +2,33 @@ import java.util.*
 
 class Parser(private val tokens: List<Token>) {
 
+    // Símbolos terminales
+    private val id = Token(TokenType.IDENTIFIER)
+    private val select = Token(TokenType.SELECT, "select")
+    private val from = Token(TokenType.FROM, "from")
+    private val distinct = Token(TokenType.DISTINCT, "distinct")
+    private val comma = Token(TokenType.COMMA, ",")
+    private val dot = Token(TokenType.DOT, ".")
+    private val asterisk = Token(TokenType.ASTERISK, "*")
+    private val eof = Token(TokenType.EOF)
 
+    // Simbolos no terminales
+    private val Q = NonTerminalSymbol.Q
+    private val D = NonTerminalSymbol.D
+    private val P = NonTerminalSymbol.P
+    private val A = NonTerminalSymbol.A
+    private val A1 = NonTerminalSymbol.A1
+    private val A2 = NonTerminalSymbol.A2
+    private val A3 = NonTerminalSymbol.A3
+    private val T = NonTerminalSymbol.T
+    private val T1 = NonTerminalSymbol.T1
+    private val T2 = NonTerminalSymbol.T2
+    private val T3 = NonTerminalSymbol.T3
+
+    // Tipo de acciones
+    private val shift = ActionType.SHIFT
+    private val reduce = ActionType.REDUCE
+    private val accept = ActionType.ACCEPT
 
 
     fun parse() {
